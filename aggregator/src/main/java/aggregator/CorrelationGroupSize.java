01635b6c6d4af956b0d3f10d29836b60a2d62f14
Christian.Henle@gmx.de <Christian.Henle@gmx.de>
2015-03-18 16:36:02 +0100

    Fehler in Choice für Fall 3 behoben, rest-json Adresse behoben

--- aggregator/src/main/java/aggregator/CorrelationGroupSize.java
+++ aggregator/src/main/java/aggregator/CorrelationGroupSize.java
@@ -15,12 +15,10 @@ public class CorrelationGroupSize implements Callable {
 	@Override
 	public Object onCall(MuleEventContext eventContext) throws Exception {
 		MuleMessage muleMessage = eventContext.getMessage();
-		//-1 for Default Bank
-		Integer availableBankNumber = 2;//Integer.parseInt(muleMessage.getInvocationProperty("bankNumber").toString())-1;
+		Integer correlationGroupSize = 2;
 		
-		muleMessage.setCorrelationGroupSize(availableBankNumber);
+		muleMessage.setCorrelationGroupSize(correlationGroupSize);
 		
-		//System.out.println("bankNumber "+muleMessage.getInvocationProperty("bankNumber"));
 		
 		
 		return muleMessage;
--- rest-json/README.md
+++ rest-json/README.md
@@ -9,7 +9,7 @@ Die Anwendung nimmt eine JSON Objekt entgegen, bildet dies auf eine Java Objekt 
 
 Der ESB wurde mithilfe der XML Datei src/main/app/rest-json.xml konfiguriert.
 
-Ein JSON Objekte wird unter der [Adresse](http://localhost:8080/foo) entgegengenommen.
+Ein JSON Objekte wird unter der [Adresse](http://localhost:8181/foo) entgegengenommen.
 Die JSON Nachricht sollte als HTTP POST im Body verschickt werden und folgendes Format einhalten:
 
 { "id":1, "email":"Test@Test.de", "username":"Test", "password":"Test", "firstname":"TestVorname", "lastname":"TestNachname" }
